b8464101d90f6ed5d657ac87eb7ea20596f3699b
Kyryl <kymachynsky.kyryl@lll.kpi.ua>
2022-10-09 15:00:50 +0300

    first commit

--- src/main/java/com/example/grafic/Application.java
+++ src/main/java/com/example/grafic/Application.java
@@ -11,7 +11,7 @@ public class Application extends javafx.application.Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("graph.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1200, 840);
-        stage.setResizable(true);
+        //stage.setResizable(true);
         stage.setTitle("Function Graph Draw");
         stage.setScene(scene);
         stage.show();
