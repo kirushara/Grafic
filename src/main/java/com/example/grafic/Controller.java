f5b0d23f9301bbe503678a2ec67262d52017c93e
Kyryl <kymachynskyi.kyryl@lll.kpi.ua>
2022-10-11 11:28:00 +0300

    new commit

--- src/main/java/com/example/grafic/Controller.java
+++ src/main/java/com/example/grafic/Controller.java
@@ -135,9 +135,9 @@ public class Controller {
         for(float x=-gWidth/2;x<gWidth/2;x++){
             line = new Line(
                     x + gWidth/2,
-                    -f(x, s, 0, '+') + gHeight/2,
+                    -f(x, s, 0) + gHeight/2,
                     x+1 +gWidth/2,
-                    -f(x+1, s, 0, '+')+ gHeight/2
+                    -f(x+1, s, 0)+ gHeight/2
             );
             line.setStroke(Color.GREEN);
             line.setStrokeWidth(2);
@@ -149,55 +149,36 @@ public class Controller {
         return x * x / 41 - x * x * x / (41 * 41);
     }
 
-    double f(double x, String s, double f, char operation){
-        int num;
-        double incr = 0;
-        int c;
-        char buff;
-        char[]array;
-        do {
-            if (s.contains("+")) {
-                c = s.indexOf("+");
-                array = s.toCharArray();
-                array[c] = '$';
-                s = new String(array);
-                s.replaceAll("$", "");
-                f(x, s, f, '+');
-            }else if (s.contains("-")) {
-                c = s.indexOf("-");
-                array = s.toCharArray();
-                array[c] = '$';
-                s = new String(array);
-                s.replaceAll("$", "");
-                f(x, s, f, '-');
-            }
+    double f(double x, String s, double f){
+        int num; double incr = 0; int c; char buff; char[]array;
+        while (!arrCheck(s)) {
             if (s.contains("x^")) {
                 c = s.indexOf("^") + 1;
                 buff = s.charAt(c);
                 num = Character.getNumericValue(buff);
                 incr = Math.pow(x, num) / Math.pow(41, num - 1);
-                if (operation == '+') {
-                    f += incr;
-                    System.out.println("1");
-                } else if (operation == '-') {
-                    f -= incr;
-                    System.out.println("2");
-                }
+                f+=incr;
                 array = s.toCharArray();
                 array[c] = '$';
                 array[c - 1] = '$';
                 array[c - 2] = '$';
                 s = new String(array);
-                s.replaceAll("$", "");
             }
-        } while (!arrCheck(s));
+            if(s.contains("+")){
+                c = s.indexOf("+");
+                array = s.toCharArray();
+                array[c] = '$';
+                s = new String(array);
+                f(x, s, f);
+            }
+        }
         return f;
     }
-    boolean arrCheck(String s){
-        char[]c=s.toCharArray();
-        int l=s.length();
-        for(int i=0;i<l;i++)
-            if(c[i]!='$') return false;
+    boolean arrCheck(String s) {
+        char[] c = s.toCharArray();
+        int l = s.length();
+        for (int i = 0; i < l; i++)
+            if (c[i] != '$') return false;
         return true;
     }
     void emptyField(boolean b){
